70f838028adc94bfbf167d8e55b308225c3f75d6
Ivanho101 <ivo@mobileinteractioncompany.com>
2023-08-15 15:20:52 +0200

    Make component of HeaderBar

---
+++ src/components/HeaderBar.svelte
@@ -0,0 +1,5 @@
+<div class="header-bar p-5 pb-48 bg-mic-pink rounded-b-[50px]">
+    <img alt="" src="" />
+    <h1 class="text-3xl text-white text-center mb-5">Dit is header tekst</h1>
+    <p class="text-white text-center">Bla bla bla</p>
+</div>
--- src/routes/campaign/[slug]/+page.svelte
+++ src/routes/campaign/[slug]/+page.svelte
@@ -1,10 +1,8 @@
 <script>
     import ProfileSection from "../../../components/ProfileSection.svelte";
+    import HeaderBar from "../../../components/HeaderBar.svelte";
 </script>
 
-<div class="header-bar p-5 pb-48 bg-mic-pink rounded-b-[50px]">
-    <img alt="" src="" />
-    <h1 class="text-3xl text-white text-center mb-5">Dit is header tekst</h1>
-    <p class="text-white text-center">Bla bla bla</p>
-</div>
+<HeaderBar />
+
 <ProfileSection />
