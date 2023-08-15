28ae5de18088c9aa06598dfacb92796eca45029c
Ivanho101 <ivo@mobileinteractioncompany.com>
2023-08-15 16:02:05 +0200

    Add badge to photo, refactor css

--- src/components/ProfileSection.svelte
+++ src/components/ProfileSection.svelte
@@ -1,9 +1,15 @@
 <div class="content-card profile p-5 bg-white m-5 -mt-16 shadow-md rounded">
-    <div class="avatar w-[150px] mx-auto -mt-[100px] shadow-md rounded-full bg-white p-0.5">
+    <div class="avatar w-[150px] mx-auto -mt-[100px] mb-8 shadow-md rounded-full bg-white p-0.5 relative">
         <img alt="" src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/MIC+Prototypes+en+Demo%27s/MIC_basic_profile-image.svg" class="min-w-full" />
+        <div class="badge absolute w-[75px] -bottom-6 -right-5">
+            <img alt="" src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/MIC+Prototypes+en+Demo's/Badges/SVG/badge_blauw_brons" />
+        </div>
     </div>
-    <div class="progress-bar my-5">
-        <h2 class="text-2xl text-center mb-2">Progress titel</h2>
+    <div class="message text-center">
+        <p>Ik collecteer omdat:<br /><span class="font-semibold">Bla bla bla</span></p>
+    </div>
+    <div class="progress-bar mt-5 mb-5">
+        <h2 class="text-2xl text-center mb-2 font-semibold">Progress titel</h2>
         <progress class="rounded-full bg-gray-100 w-full" value="5" max="10"></progress>
     </div>
     <div class="stats flex justify-center">
