5ad45c0d341f5ad70d6ee35f4c65bacdf0fff714
Ivanho101 <ivo@mobileinteractioncompany.com>
2023-08-15 15:07:42 +0200

    Make component of ProfileSection

---
+++ src/components/ProfileSection.svelte
@@ -0,0 +1,19 @@
+<div class="content-card profile p-5 bg-white m-5 -mt-16 shadow-md rounded">
+    <div class="avatar w-[150px] mx-auto -mt-[100px] shadow-md rounded-full bg-white p-0.5">
+        <img alt="" src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/MIC+Prototypes+en+Demo%27s/MIC_basic_profile-image.svg" class="min-w-full" />
+    </div>
+    <div class="progress-bar my-5">
+        <h2 class="text-2xl text-center mb-2">Progress titel</h2>
+        <progress class="rounded-full bg-gray-100 w-full" value="5" max="10"></progress>
+    </div>
+    <div class="stats flex justify-center">
+        <div class="stats-item bg-gray-100 p-2 pr-4 rounded-l-lg border-r text-center w-[90px]">
+            <p>Bedrag</p>
+            <h3 class="text-lg font-semibold">€0,00</h3>
+        </div>
+        <div class="stats-item bg-gray-100 p-2 pl-4 rounded-r-lg border-l text-center w-[90px]">
+            <p>Donaties</p>
+            <h3 class="text-lg font-semibold">0</h3>
+        </div>
+    </div>
+</div>
