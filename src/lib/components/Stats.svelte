a268e1a7f9ceb191109141122e19375f77ad29c8
Ivanho101 <ivo@mobileinteractioncompany.com>
2023-08-22 16:32:38 +0200

    Rename various files, move compontents to lib, add parse number function

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-    export let statsAmount: number = 0.00;
-    export let statsDonations: number = 0;
+	import { parseNumber } from "$lib/utils";
+
+    export let current = 0.00;
+    export let donations = 0;
 </script>
 
 <div class="stats flex justify-center">
     <div class="stats-item bg-mic-light p-2 pr-4 rounded-l-lg border-r text-center w-[90px]">
         <p>Bedrag</p>
-        <h3 class="text-lg font-semibold">€{statsAmount}</h3>
+        <h3 class="text-lg font-semibold">€{parseNumber(current)}</h3>
     </div>
     <div class="stats-item bg-mic-light p-2 pl-4 rounded-r-lg border-l text-center w-[90px]">
         <p>Donaties</p>
-        <h3 class="text-lg font-semibold">{statsDonations}</h3>
+        <h3 class="text-lg font-semibold">{donations}</h3>
     </div>
 </div>
